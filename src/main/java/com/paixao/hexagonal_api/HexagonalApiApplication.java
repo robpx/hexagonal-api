914dcee1cf9ec790833dcf6a8d1d27e8e563f813
Robson Paixão <robson.emanuelpx@gmail.com>
2025-05-19 06:42:09 -0300

    chore: initial configs

---
+++ src/main/java/com/paixao/hexagonal_api/HexagonalApiApplication.java
@@ -0,0 +1,13 @@
+package com.paixao.hexagonal_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HexagonalApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HexagonalApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/paixao/hexagonal_api/HexagonalApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.paixao.hexagonal_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HexagonalApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
